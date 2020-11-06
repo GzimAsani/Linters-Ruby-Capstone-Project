60758fe2afc153acba9928acd6331ee60b715798
Gzim <gzimasanni@gmail.com>
2020-11-06 20:21:10 +0100

    Fix based on reviews

--- lib/rules.rb
+++ lib/rules.rb
@@ -55,8 +55,8 @@ module Linter
     unless rows.size.positive? && rows[-1].include?("\n")
       @last_line_error << "Expecting blank line after end [#{last_row}]"
     end
-     unless rows.size.positive? && rows[-1] == "\n"
-     end
+    unless rows.size.positive? && rows[-1] == "\n"
+    end
 
     x = -1
     @last_line_error << "Unexpected blank line after [#{last_row + 1}]" if rows[x] == "\n"
